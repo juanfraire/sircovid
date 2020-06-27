7fb3823583e9e328fee8296e48e47794b4bf9b3f
Juan <juanfraire@gmail.com>
2020-06-27 20:50:49 -0300

    emprolijamiento del código

--- sircovid/sirCovid.go
+++ sircovid/sirCovid.go
@@ -13,49 +13,59 @@ import (
 )
 
 const (
+	// game
 	screenWidth  = 999
 	screenHeight = 687
-	tileSize     = 16
-	tileXNum     = 48
+
+	// viejo
+	viejoFrameWidth  = 32
+	viejoFrameHeight = 48
+
+	// tiles
+	tileSize = 16
+	tileXNum = 48
 )
 
 var (
-	tilesImage  *ebiten.Image
-	smokeImg    *ebiten.Image
-	viejoImg    *ebiten.Image
-	frameOX     = 0
-	frameOY     = 96
-	frameWidth  = 32
-	frameHeight = 48
-	frameNum    = 1
-	x           = float64(25)
-	y           = float64(375)
-	x1          = x
-	y1          = y
-	nCount      = float64(screenWidth + 1200)
-	smokeX      = float64(screenWidth + 1200)
-	nube        = nCount / 2.2012
-	smokeY      = float64(800)
-	alpha       float64
-	tmp         float64
-	movx        int
-	movy        int
-	err         error
-	vidas       = 3
-	vidtime     int
+	// imágenes
+	imgTiles *ebiten.Image
+	imgNube  *ebiten.Image
+	imgViejo *ebiten.Image
+
+	// viejo
+	viejoFrameOX  = 0
+	viejoFrameOY  = 96
+	viejoFrameNum = 1
+	viejoX        = float64(25)
+	viejoY        = float64(375)
+	viejoMovX     int
+	viejoMovY     int
+
+	// nube
+	nubeX       = float64(screenWidth + 1200)
+	nubeY       = float64(800)
+	nubeAlpha   float64
+	nubeAlphaUp bool
+
+	// vidas
+	vidas   = 3
+	vidtime int
+
+	err error
 )
 
+// init carga los datos
 func init() {
 
-	tilesImage, _, err = ebitenutil.NewImageFromFile("sircovid/data/city2.png", ebiten.FilterDefault)
+	imgTiles, _, err = ebitenutil.NewImageFromFile("sircovid/data/city2.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
-	viejoImg, _, err = ebitenutil.NewImageFromFile("sircovid/data/viejo.png", ebiten.FilterDefault)
+	imgViejo, _, err = ebitenutil.NewImageFromFile("sircovid/data/viejo.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
-	smokeImg, _, err = ebitenutil.NewImageFromFile("sircovid/data/smoke.png", ebiten.FilterDefault)
+	imgNube, _, err = ebitenutil.NewImageFromFile("sircovid/data/smoke.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,8 +73,7 @@ func init() {
 
 // Game es la estructura del juego
 type Game struct {
-	layers [][]int
-	count  int
+	count int
 }
 
 ////////////////////////////
@@ -74,96 +83,95 @@ type Game struct {
 // Update se llama 60 veces por segundo
 func (g *Game) Update(screen *ebiten.Image) error {
 
+	// game counter
 	g.count++
-	nCount--
-	nube = nCount / 2.57
-	if nCount == 0 {
-		nCount = smokeX
-	}
-	covid()
-	mover()
-	translado()
-	restricciones()
-	go vida()
+
+	// nube
+	moverNube()
+
+	// viejo
+	moverViejo()
+
+	// vida
+	vida()
+
 	return nil
 }
 
-// mover recorte de imagen segun la direccion de movimiento del viejo
-func mover() {
+// moverViejo recorte de imagen segun la direccion de movimiento del viejo
+func moverViejo() {
+	// leer tecla
 	switch {
-	case inpututil.IsKeyJustPressed(ebiten.KeyRight) && movy != 1 && movy != 2:
-		frameOY = 96
-		frameNum = 3
-		movx = 1
-	case inpututil.IsKeyJustReleased(ebiten.KeyRight) && movy != 1 && movy != 2:
-		frameNum = 1
-		movx = 0
-	case inpututil.IsKeyJustPressed(ebiten.KeyLeft) && movy != 1 && movy != 2:
-		frameOY = 48
-		frameNum = 3
-		movx = 2
-	case inpututil.IsKeyJustReleased(ebiten.KeyLeft) && movy != 1 && movy != 2:
-		frameNum = 1
-		movx = 0
-	case inpututil.IsKeyJustPressed(ebiten.KeyUp) && movx != 1 && movx != 2:
-		frameOY = 144
-		frameNum = 3
-		movy = 1
-	case inpututil.IsKeyJustReleased(ebiten.KeyUp) && movx != 1 && movx != 2:
-		frameNum = 1
-		movy = 0
-	case inpututil.IsKeyJustPressed(ebiten.KeyDown) && movx != 1 && movx != 2:
-		frameOY = 0
-		frameNum = 3
-		movy = 2
-	case inpututil.IsKeyJustReleased(ebiten.KeyDown) && movx != 1 && movx != 2:
-		frameNum = 1
-		movy = 0
+	case inpututil.IsKeyJustPressed(ebiten.KeyRight) && viejoMovY != 1 && viejoMovY != 2:
+		viejoFrameOY = 96
+		viejoFrameNum = 3
+		viejoMovX = 1
+	case inpututil.IsKeyJustReleased(ebiten.KeyRight) && viejoMovY != 1 && viejoMovY != 2:
+		viejoFrameNum = 1
+		viejoMovX = 0
+	case inpututil.IsKeyJustPressed(ebiten.KeyLeft) && viejoMovY != 1 && viejoMovY != 2:
+		viejoFrameOY = 48
+		viejoFrameNum = 3
+		viejoMovX = 2
+	case inpututil.IsKeyJustReleased(ebiten.KeyLeft) && viejoMovY != 1 && viejoMovY != 2:
+		viejoFrameNum = 1
+		viejoMovX = 0
+	case inpututil.IsKeyJustPressed(ebiten.KeyUp) && viejoMovX != 1 && viejoMovX != 2:
+		viejoFrameOY = 144
+		viejoFrameNum = 3
+		viejoMovY = 1
+	case inpututil.IsKeyJustReleased(ebiten.KeyUp) && viejoMovX != 1 && viejoMovX != 2:
+		viejoFrameNum = 1
+		viejoMovY = 0
+	case inpututil.IsKeyJustPressed(ebiten.KeyDown) && viejoMovX != 1 && viejoMovX != 2:
+		viejoFrameOY = 0
+		viejoFrameNum = 3
+		viejoMovY = 2
+	case inpututil.IsKeyJustReleased(ebiten.KeyDown) && viejoMovX != 1 && viejoMovX != 2:
+		viejoFrameNum = 1
+		viejoMovY = 0
 	}
-}
 
-// traslado del viejo coordenadas en x-y
-func translado() {
-	x1 = x
-	y1 = y
+	// transladar viejo
+	var viejoX1 = viejoX
+	var viejoY1 = viejoY
 	switch {
-	case movx == 1:
-		x += 1.3
-	case movx == 2:
-		x -= 1.3
-	case movy == 1:
-		y -= 1.3
-	case movy == 2:
-		y += 1.3
+	case viejoMovX == 1:
+		viejoX += 1.3
+	case viejoMovX == 2:
+		viejoX -= 1.3
+	case viejoMovY == 1:
+		viejoY -= 1.3
+	case viejoMovY == 2:
+		viejoY += 1.3
 	}
-}
 
-func restricciones() {
+	// restringir viejo
 	switch {
-	case y < 300*1.3 && x > 20*1.3 && x < 214*1.3:
-		x = x1
-		y = y1
-		frameNum = 1
-	case y < 130*1.3 && x > 214*1.3 && x < 768*1.3:
-		x = x1
-		y = y1
-		frameNum = 1
-	case y < 270*1.3 && x > 240*1.3 && x < 610*1.3:
-		x = x1
-		y = y1
-		frameNum = 1
-	case y < 270*1.3 && x > 675*1.3 && x < 768*1.3:
-		x = x1
-		y = y1
-		frameNum = 1
-	case y > 335*1.3 && y < 528*1.3 && x > 40*1.3 && x < 350*1.3:
-		x = x1
-		y = y1
-		frameNum = 1
-	case y > 310*1.3 && y < 450*1.3 && x > 390*1.3 && x < 630*1.3:
-		x = x1
-		y = y1
-		frameNum = 1
+	case viejoY < 300*1.3 && viejoX > 20*1.3 && viejoX < 214*1.3:
+		viejoX = viejoX1
+		viejoY = viejoY1
+		viejoFrameNum = 1
+	case viejoY < 130*1.3 && viejoX > 214*1.3 && viejoX < 768*1.3:
+		viejoX = viejoX1
+		viejoY = viejoY1
+		viejoFrameNum = 1
+	case viejoY < 270*1.3 && viejoX > 240*1.3 && viejoX < 610*1.3:
+		viejoX = viejoX1
+		viejoY = viejoY1
+		viejoFrameNum = 1
+	case viejoY < 270*1.3 && viejoX > 675*1.3 && viejoX < 768*1.3:
+		viejoX = viejoX1
+		viejoY = viejoY1
+		viejoFrameNum = 1
+	case viejoY > 335*1.3 && viejoY < 528*1.3 && viejoX > 40*1.3 && viejoX < 350*1.3:
+		viejoX = viejoX1
+		viejoY = viejoY1
+		viejoFrameNum = 1
+	case viejoY > 310*1.3 && viejoY < 450*1.3 && viejoX > 390*1.3 && viejoX < 630*1.3:
+		viejoX = viejoX1
+		viejoY = viejoY1
+		viejoFrameNum = 1
 	}
 }
 
@@ -172,7 +180,7 @@ func vida() {
 	if vidas == vidtime {
 		time.Sleep(1 * time.Second)
 	}
-	if int(x) > int(nube) && int(x) < int(nube+50) && int(y) < 428 && int(y) > 298 {
+	if int(viejoX) > int(nubeX) && int(viejoX) < int(nubeX+50) && int(viejoY) < 428 && int(viejoY) > 298 {
 		vidas = vidas - 1
 		vidtime = vidas
 		fmt.Println(vidas)
@@ -180,17 +188,23 @@ func vida() {
 
 }
 
-// covid aumenta y disminuye transparencia de la nube (alpha)
-func covid() {
-	switch {
-	case tmp == 0 && alpha < 1:
-		alpha += .003
-	case alpha > 1 && tmp != 1:
-		tmp = 1
-	case tmp == 1 && alpha > 0:
-		alpha -= .003
-	case alpha <= 0:
-		tmp = 0
+// nubeCovid aumenta y disminuye transparencia de la nube (alpha)
+func moverNube() {
+	nubeX--
+
+	// actualizar alpha
+	if nubeAlphaUp {
+		nubeAlpha += .003
+	} else {
+		nubeAlpha -= .003
+	}
+
+	// cambiar dirección alpha
+	if nubeAlpha < 1 {
+		nubeAlphaUp = true
+	}
+	if nubeAlpha > 1 {
+		nubeAlphaUp = false
 	}
 }
 
@@ -200,23 +214,25 @@ func covid() {
 
 // Draw dibuja la pantalla 60 veces por segundo
 func (g *Game) Draw(screen *ebiten.Image) {
+
+	// dubujar fondo
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(1.3, 1.3)
-	screen.DrawImage(tilesImage, op)
+	screen.DrawImage(imgTiles, op)
 
+	// dibujar viejo
 	op.GeoM.Scale(.7, .7)
-	op.GeoM.Translate(x, y)
-	i := (g.count / 7) % frameNum
-	sx, sy := frameOX+i*frameWidth, frameOY
-	screen.DrawImage(viejoImg.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), op)
+	op.GeoM.Translate(viejoX, viejoY)
+	i := (g.count / 7) % viejoFrameNum
+	sx, sy := viejoFrameOX+i*viejoFrameWidth, viejoFrameOY
+	screen.DrawImage(imgViejo.SubImage(image.Rect(sx, sy, sx+viejoFrameWidth, sy+viejoFrameHeight)).(*ebiten.Image), op)
 
+	// dibujar nube
 	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(nCount, smokeY)
-	op.ColorM.Scale(1, 3, 2, alpha)
+	op.GeoM.Translate(nubeX, nubeY)
+	op.ColorM.Scale(1, 3, 2, nubeAlpha)
 	op.GeoM.Scale(.4, .4)
-	screen.DrawImage(smokeImg, op)
-
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
+	screen.DrawImage(imgNube, op)
 }
 
 // Layout manja las dimensiones de pantalla
@@ -229,9 +245,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 ////////////////////////////
 
 func main() {
-
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Tiles (Ebiten Demo)")
+	ebiten.SetWindowTitle("Sircovid")
+
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
