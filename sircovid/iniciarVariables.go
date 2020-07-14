7db458bc163c13296d87e9835dcd54889ffc7570
LucasCarranzaBertarelli <66894042+LucasCarranzaBertarelli@users.noreply.github.com>
2020-07-14 20:41:45 -0300

    simplificar variable

--- sircovid/iniciarVariables.go
+++ sircovid/iniciarVariables.go
@@ -10,46 +10,6 @@ func init() {
 }
 
 func iniciarVariables() {
-	// hombre.FrameOX = 0
-	// hombre.FrameOY = 48
-	// hombre.FrameNum = 1
-	// hombre.X = float64(350)
-	// hombre.Y = float64(290)
-	// hombre.FrameWidth = 32
-	// hombre.FrameHeight = 48
-
-	// mujer.FrameOX = 0
-	// mujer.FrameOY = 48
-	// mujer.FrameNum = 1
-	// mujer.X = float64(screenWidth - 50)
-	// mujer.Y = float64(290)
-	// mujer.FrameWidth = 32
-	// mujer.FrameHeight = 48
-
-	// 	hombre.FrameOX = 0
-	// 	hombre.FrameOY = 48
-	// 	hombre.FrameNum = 1
-	// 	hombre.X = float64(350)
-	// 	hombre.Y = float64(290)
-	// 	hombre.FrameWidth = 32
-	// 	hombre.FrameHeight = 48
-	// 	hombre.num = rand.Intn(5)
-	// 	hombre.cambio = rand.Intn(100) + 100
-
-	// 	mujer.FrameOX = 0
-	// 	mujer.FrameOY = 48
-	// 	mujer.FrameNum = 1
-	// 	mujer.X = float64(screenWidth - 50)
-	// 	mujer.Y = float64(290)
-	// 	mujer.FrameWidth = 32
-	// 	mujer.FrameHeight = 48
-	// 	mujer.num = rand.Intn(5)
-	// 	mujer.cambio = rand.Intn(100) + 50
-
-	// 	//enemigos
-	// 	enemigos1.humanos = hombre
-	// 	enemigos2.humanos = mujer
-	// >>>>>>> 3bb2d837a2760e6774f74a8c03e827626696b24f
 
 	//nube
 	nube1.nubeX = float64(rand.Intn(screenWidth) + 300)
