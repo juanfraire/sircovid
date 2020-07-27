9713645d6ad408917f777a3b20642b22942df0de
LucasCarranzaBertarelli <66894042+LucasCarranzaBertarelli@users.noreply.github.com>
2020-07-27 19:54:12 -0300

    win

--- sircovid/Game.go
+++ sircovid/Game.go
@@ -109,7 +109,7 @@ func siguienteNivel(p player) player {
 		fondo.Pause()
 		fondo.Rewind()
 	}
-	if p.X[0] > 752 && p.X[0] < 762 && p.Y[0] < -40 {
+	if p.X[0] > 751 && p.X[0] < 763 && p.Y[0] < -40 {
 		ModeWin = true
 		fondo.Pause()
 	}
