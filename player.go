6bc9bb18e0a6d7ff9980b32acb26495a0d9092fb
LucasCarranzaBertarelli <66894042+LucasCarranzaBertarelli@users.noreply.github.com>
2020-08-26 15:14:51 -0300

    bugs

--- player.go
+++ player.go
@@ -395,7 +395,7 @@ func vida(h humanos, p player, b Objetos, pl Objetos) (player, Objetos, Objetos)
 	}
 
 	//inmune con barbijo o alchol en gel
-	if p.X[0]+wth > b.X && p.X[0] < b.X+barWscale+20 && p.Y[0]+hgt-45 > b.Y && p.Y[0]+hgt-45 < b.Y+barHScale {
+	if !casita && !banco && p.X[0]+wth > b.X && p.X[0] < b.X+barWscale+20 && p.Y[0]+hgt-45 > b.Y && p.Y[0]+hgt-45 < b.Y+barHScale {
 		sBarbijo.Play()
 		sBarbijo.Rewind()
 		b.X = 1500
@@ -413,7 +413,7 @@ func vida(h humanos, p player, b Objetos, pl Objetos) (player, Objetos, Objetos)
 	}
 
 	//gana/pierde vida
-	if p.X[0]+wth > pl.X && p.X[0] < pl.X+plasmaWScale+20 && p.Y[0]+hgt > pl.Y && p.Y[0]+hgt < pl.Y+plasmaHScale+20 {
+	if !casita && !banco && p.X[0]+wth > pl.X && p.X[0] < pl.X+plasmaWScale+20 && p.Y[0]+hgt > pl.Y && p.Y[0]+hgt < pl.Y+plasmaHScale+20 {
 		sBarbijo.Play()
 		sBarbijo.Rewind()
 		p.vidas++
